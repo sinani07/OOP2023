3e05b460f94405cc125061ea327f2605820abfc9
Ardian Sinani <ardian@MacBook-Air-od-Ardian.local>
2023-10-15 18:30:22 +0200

    adding dependency

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,22 @@
-package org.example;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner reader = new Scanner(System.in);
+        String correctPassword = "Ardian123";
+        boolean passwordCorrect = false;
+        while (!passwordCorrect) {
+            System.out.print("Enter your password: ");
+            String userPassword = reader.nextLine();
+            if (userPassword.equals(correctPassword)) {
+                System.out.println("You entered the correct password! karrot!");
+                passwordCorrect = true;
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+            } else {
+                System.out.println("Incorrect password, try again!");
+            }
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+
         }
+
     }
 }
