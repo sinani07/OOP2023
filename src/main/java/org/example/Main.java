afb70cd2eefff0178cdddd515986d685d3f1b4b0
Ardian Sinani <ardian@MacBook-Air-od-Ardian.local>
2023-10-15 20:24:29 +0200

    adding dependency

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        String correctPassword = "Ardian123";
+        /*String correctPassword = "Ardian123";
         boolean passwordCorrect = false;
         while (!passwordCorrect) {
             System.out.print("Enter your password: ");
@@ -10,13 +10,19 @@ public class Main {
             if (userPassword.equals(correctPassword)) {
                 System.out.println("You entered the correct password! karrot!");
                 passwordCorrect = true;
-
             } else {
                 System.out.println("Incorrect password, try again!");
             }
-
-
+        }*/
+        int sum = 0;
+        int read;
+        int i=0;
+        while(i<3){
+            System.out.print("Enter a number: ");
+            read= reader.nextInt();
+            sum+=read;
+            i++;
         }
-
+        System.out.println("Sum is: "+ sum);
     }
 }
