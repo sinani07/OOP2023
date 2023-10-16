bbcea3567e7354f74c9820525d8a4053f9fb7105
Ardian Sinani <ardian@MacBook-Air-od-Ardian.local>
2023-10-16 15:10:51 +0200

    week2

---
+++ src/main/java/Week2/Exercise1.java
@@ -0,0 +1,21 @@
+package Week2;
+
+import java.util.Scanner;
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String correctPassword = "Ardian123";
+        boolean passwordCorrect = false;
+        while (!passwordCorrect) {
+            System.out.print("Enter your password: ");
+            String userPassword = reader.nextLine();
+            if (userPassword.equals(correctPassword)) {
+                System.out.println("You entered the correct password! karrot!");
+                passwordCorrect = true;
+            } else {
+                System.out.println("Incorrect password, try again!");
+            }
+        }
+
+    }
+}
